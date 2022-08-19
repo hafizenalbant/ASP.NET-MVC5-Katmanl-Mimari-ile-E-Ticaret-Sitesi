f0e282d84be4aee3f7e7d540742709bf47cef551
hafizenalbant <61289808+hafizenalbant@users.noreply.github.com>
2022-08-19 14:53:54 +0300

    ASP.NET MVC5 Katmanlı Mimari ile E-Ticaret Sitesi

---
+++ E-Shop/DataAccessLayer/Migrations/202208191049171_data.cs
@@ -0,0 +1,22 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class data : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Users", "Email", c => c.String(nullable: false, maxLength: 50));
+            AlterColumn("dbo.Users", "Password", c => c.String(nullable: false, maxLength: 10));
+            AlterColumn("dbo.Users", "RePassword", c => c.String(nullable: false, maxLength: 10));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Users", "RePassword", c => c.String());
+            AlterColumn("dbo.Users", "Password", c => c.String());
+            AlterColumn("dbo.Users", "Email", c => c.String());
+        }
+    }
+}
---
+++ E-Shop/E-Shop/Controllers/AdminController.cs
@@ -0,0 +1,49 @@
+﻿using DataAccessLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList.Mvc;
+using PagedList;
+
+namespace E_Shop.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {     
+        // GET: Admin
+        DataContext DB = new DataContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Comment(int sayfa=1)
+        {
+            return View(DB.Comments.ToList().ToPagedList(sayfa,3));
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var delete=DB.Comments.Where(x=>x.Id==id).FirstOrDefault();
+            DB.Comments.Remove(delete);
+            DB.SaveChanges();
+            return RedirectToAction("Comment");
+        }
+
+        public ActionResult UserList()
+        {
+            var user=DB.Users.Where(x=>x.Role=="User").ToList();
+            return View(user);
+        }
+
+        public ActionResult UserDelete(int id)
+        {
+            var userid=DB.Users.Where(x=>x.Id == id).FirstOrDefault();
+            DB.Users.Remove(userid);
+            DB.SaveChanges();
+            return RedirectToAction("UserList");
+        }
+    }
+}
